942f783f1aff49e97eeb8a1ec78a55cb702375f2
Marius Liaaen <marius@Anyas-iMac.local>
2023-01-10 16:20:32 +0100

    correct files

--- steps.py
+++ steps.py
@@ -34,16 +34,32 @@ def step(context, document_name):
     document = find_visible_document(document_name)
     test.verify(document != None)
 
+@Given("the |any| folder is  visible in My Files")
+def step(context, folder_name):
+    folder = find_visible_folder(folder_name)
+    test.verify(folder != None)
 
 #### When statements (exercising behaviour) ####
 @When("the user long presses the '|any|' document")
 def step(context, document_name):
     squish.longMouseClick(find_visible_document(document_name))
 
+@When("the user long presses the '|any|' folder")
+def step(context, document_name):
+    squish.longMouseClick(find_visible_folder(folder_name))
+
+@When("the user short presses the '|any|' folder")
+def step(context, folder_name):
+    squish.MouseClick(find_visible_folder(folder_name))
 
 @When("the user selects move from the drawer")
 def step(context):
     squish.mouseClick(squish.waitForObject(names.drawer_move))
+
+@When("the user selectes move_here from the panel")
+def step(context):
+    squish.mouseClick(squish.waitForObject(names.movepanel_move_here))
+
 
 
 #### Then statements (verifying outcome) ####
@@ -52,3 +68,12 @@ def step(context):
     squish.waitForObject(names.movePanel_label)
     squish.waitForObject(names.movePanel_cancel_move)
     squish.waitForObject(names.movePanel_move)
+
+Then 
+
+@Then("the user should see the file |Test|' in the folder")
+def step(context, document_name):
+    find_visible_document(document_name):
+    
+
+
--- test.feature
+++ test.feature
@@ -17,7 +17,22 @@ Feature: Move files and folders
         Then the user should be prompted with the move panel
 
     Scenario: Move single file to folder
-        # Complete this scenario!
+        
+        Given the 'Test' document is visible in My Files
+        And  the 'Some folder' folder is  visible in My Files
+        When the user long presses the 'Test' document
+        And the user selects move from the drawer
+        And the user short presses the ´Some folder' folder
+        And the user presses  move_here in panel
+        Then the user should see the file 'Test' the folder
 
     Scenario: Move file and folder to folder
-        # Complete this scenario!
+    
+        Given the 'Some folder' folder is visible in My Files
+        And the 'Somme other folder' folder is visible in My Files
+        When the user long presses the 'Some folder' folder
+        And the user selects move from the drawer
+        And the user short presses the 'Some other folder' folder 
+        And the user selects move_here in the panel
+        Then the user should see the 'Some folder' 
+
